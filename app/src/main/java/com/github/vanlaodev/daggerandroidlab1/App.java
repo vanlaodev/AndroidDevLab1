0ceb047e5385ea8bd02de1f8e668f264904babf7
Van Lao <vanlaodev@gmail.com>
2017-07-05 17:44:24 +0800

    update

--- app/src/main/java/com/github/vanlaodev/daggerandroidlab1/App.java
+++ app/src/main/java/com/github/vanlaodev/daggerandroidlab1/App.java
@@ -1,9 +1,23 @@
 package com.github.vanlaodev.daggerandroidlab1;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
 public class App extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerAppComponent.builder().create(this);
